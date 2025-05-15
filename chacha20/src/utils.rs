dabd7cd02b915c1dc8a93a436b9a9363901ace06
gabijacob <g186087@dac.unicamp.br>
2025-05-15 17:15:22 -0300

    changing function name

--- chacha20/src/utils.rs
+++ chacha20/src/utils.rs
@@ -1,4 +1,4 @@
-pub fn imprime_bloco(label: &str, bloco: &[u32; 16]) {
+pub fn print_block(label: &str, bloco: &[u32; 16]) {
     println!("{}:", label);
     for i in 0..16 {
         print!("{:08x} ", bloco[i]);
